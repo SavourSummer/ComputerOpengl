d69fb50f3d929b19338e71bfb3c93752b5094dee
SavourSummer <271044354@qq.com>
2024-03-13 17:11:03 +0800

    P30

--- OpenGL/OpenGL/src/Shader.h
+++ OpenGL/OpenGL/src/Shader.h
@@ -22,6 +22,7 @@ public:
     void UnBind()const;
     void SetUniform1i(const std::string& name, int value);
     void SetUniform1f(const std::string& name, float value);
+    void SetUniform1iv(const std::string& name, int count, int* value);
     void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);//v1,v2,v3用于设定颜色
     void SetUniformMat4f(const std::string& name, const glm::mat4& matrix);
 private:
